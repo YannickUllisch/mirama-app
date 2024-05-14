f027fa3a9ab6e014b33ed117b405a560869869e7
YannickUllisch <yannicku01@gmail.com>
2024-05-14 23:45:27 +0200

    feat: card

--- app/api/db/projekt/[id]/route.ts
+++ app/api/db/projekt/[id]/route.ts
@@ -1,24 +1,24 @@
 import { db } from '@src/lib/db'
 import { auth } from '@/auth'
 
-export const GET = auth(async (req) => {
-  try {
-    const session = req.auth
-    const id = req.nextUrl.const
-    response = await db.project.findMany({
-      where: {
-        teamId: session?.user.teamId,
-      },
-      include: {
-        managedBy: true,
-      },
-      orderBy: {
-        endDate: 'desc',
-      },
-    })
+// export const GET = auth(async (req) => {
+//   try {
+//     const session = req.auth
+//     const id = req.nextUrl.const
+//     response = await db.project.findMany({
+//       where: {
+//         teamId: session?.user.teamId,
+//       },
+//       include: {
+//         managedBy: true,
+//       },
+//       orderBy: {
+//         endDate: 'desc',
+//       },
+//     })
 
-    return new Response(JSON.stringify(response))
-  } catch (err) {
-    return new Response(JSON.stringify(err))
-  }
-})
+//     return new Response(JSON.stringify(response))
+//   } catch (err) {
+//     return new Response(JSON.stringify(err))
+//   }
+// })
