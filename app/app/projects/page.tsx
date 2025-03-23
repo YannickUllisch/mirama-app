98a4c9395d475fab784621a1fb160aaef8925af0
YannickUllisch <yannicku01@gmail.com>
2025-03-23 19:12:43 +0100

    feat: setting up socketIO store

--- app/app/projects/page.tsx
+++ app/app/projects/page.tsx
@@ -2,10 +2,11 @@
 import type { Project, ProjectUser, User } from '@prisma/client'
 import { DataTable } from '@src/components/Tables/DataTable'
 import { TableCell, TableFooter, TableRow } from '@src/components/ui/table'
-import React from 'react'
+import React, { useEffect } from 'react'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
 import { ProjectColumns } from './columns'
+import useSocketStore from '@store/socket'
 
 const ProjectsPage = () => {
   // Fetching Project Data
@@ -31,7 +32,34 @@ const ProjectsPage = () => {
     },
   })
 
+  const { socket, connect, emit } = useSocketStore() // deconstructing socket and its method from socket store
   const { data: session } = useSession({ required: true })
+
+  useEffect(() => {
+    connect()
+  }, [connect])
+
+  // Effect for listening to team status updates
+  useEffect(() => {
+    if (!session?.user.id || !socket) return
+    // Only listen to the `team_status` event once per socket connection
+    const onTeamStatusUpdate = (data: any) => {
+      console.log('Updated team status:', data)
+    }
+
+    // Emit the join_team event after the connection is established
+    emit('join_team', {
+      userId: session?.user.id,
+      teamId: session?.user.teamId,
+    })
+
+    socket.on('team_status', onTeamStatusUpdate)
+
+    // Clean up the listener when the component is unmounted or socket changes
+    return () => {
+      socket.off('team_status', onTeamStatusUpdate)
+    }
+  }, [socket, session, socket, emit]) // This effect runs when the socket is available
 
   const { data: users } = useSWR<User[]>('team/member')
 
