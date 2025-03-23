8a69cc9c156b63321b497b035aac524299b10c03
YannickUllisch <yannicku01@gmail.com>
2025-03-23 19:48:00 +0100

    fix: trying to optimize socket connection

--- app/app/projects/page.tsx
+++ app/app/projects/page.tsx
@@ -32,34 +32,14 @@ const ProjectsPage = () => {
     },
   })
 
-  const { socket, connect, emit } = useSocketStore() // deconstructing socket and its method from socket store
+  const { connect } = useSocketStore() // deconstructing socket and its method from socket store
   const { data: session } = useSession({ required: true })
 
   useEffect(() => {
-    connect()
-  }, [connect])
-
-  // Effect for listening to team status updates
-  useEffect(() => {
-    if (!session?.user.id || !socket) return
-    // Only listen to the `team_status` event once per socket connection
-    const onTeamStatusUpdate = (data: any) => {
-      console.log('Updated team status:', data)
+    if (session?.user?.id) {
+      connect(session)
     }
-
-    // Emit the join_team event after the connection is established
-    emit('join_team', {
-      userId: session?.user.id,
-      teamId: session?.user.teamId,
-    })
-
-    socket.on('team_status', onTeamStatusUpdate)
-
-    // Clean up the listener when the component is unmounted or socket changes
-    return () => {
-      socket.off('team_status', onTeamStatusUpdate)
-    }
-  }, [socket, session, socket, emit]) // This effect runs when the socket is available
+  }, [session, connect])
 
   const { data: users } = useSWR<User[]>('team/member')
 
