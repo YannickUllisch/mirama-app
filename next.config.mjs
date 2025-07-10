b2f62fb7a3955c30b1203f90b21a6e90841beb9c
YannickUllisch <yannicku01@gmail.com>
2025-07-10 15:29:36 +0200

    chore: removing mdx 2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['ts', 'tsx'],
   output: 'standalone',
 }
