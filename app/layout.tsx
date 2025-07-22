70bcbb4498338e254ea8d381dc94ec676e9ea4a9
YannickUllisch <yannicku01@gmail.com>
2025-07-22 23:35:44 +0200

    chore: smaller cleanups and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import { ThemeProvider } from '@src/components/Wrappers/ThemeProvider'
 import './globals.css'
-import { Toaster } from 'sonner'
 import type { Metadata } from 'next'
-import NextTopLoader from 'nextjs-toploader'
 import { DM_Sans } from 'next/font/google'
+import { Toaster } from 'sonner'
 
 const dmMono = DM_Sans({
   subsets: ['latin'],
@@ -21,7 +20,6 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     <html suppressHydrationWarning lang="en" className={dmMono.variable}>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <body className="bg-background">
-        <NextTopLoader zIndex={99999} color="#FF5F46" showSpinner={false} />
         <Toaster />
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
